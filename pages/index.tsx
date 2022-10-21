4d138b1c6ddb8791cec960e68c2d6b3820fc6b94
hello-kozloff <hello.kozloff@gmail.com>
2022-10-21 21:52:27 +0300

    Configure next-app template typescript

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Homepage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Homepage
